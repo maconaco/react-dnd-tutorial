3ef27f07ed69095785dbbb7a4eedac23564be1ad
macololidoll <makomakopg@gmail.com>
2020-12-09 17:42:41 +0900

    feat: table row component

--- src/Table.js
+++ src/Table.js
@@ -9,34 +9,21 @@ export default function Table() {
             <th>アーティスト名</th>
             <th>楽曲URL</th>
           </tr>
-          <TableRow songTitle='イカサマダンス' artistName='まふまふ' songUrl=' https://www.nicovideo.jp/watch/sm37812606' />
-          <tr style={{ textAlign: 'center'}}>
-            <td>イカサマダンス</td>
-            <td>まふまふ</td>
-            <td>
-              <a href="https://www.nicovideo.jp/watch/sm37812606" style={{textDecoration: 'none'}}>
-                  https://www.nicovideo.jp/watch/sm37812606
-              </a>
-            </td>
-          </tr>
-          <tr style={{backgroundColor: '#eee', textAlign: 'center'}}>
-            <td>アンチジョーカー</td>
-            <td>マイキP</td>
-            <td>
-              <a href="https://www.nicovideo.jp/watch/sm37478808" style={{textDecoration: 'none'}}>
-                https://www.nicovideo.jp/watch/sm37478808
-              </a>
-            </td>
-          </tr>
-          <tr style={{ textAlign: 'center'}}>
-            <td>potatoになっていく</td>
-            <td>Neru</td>
-            <td>
-              <a href="https://www.nicovideo.jp/watch/sm36620941" style={{textDecoration: 'none'}}>
-                https://www.nicovideo.jp/watch/sm36620941
-              </a>
-              </td>
-          </tr>
+          <TableRow
+            songTitle='イカサマダンス'
+            artistName='まふまふ'
+            songUrl='https://www.nicovideo.jp/watch/sm37812606'
+          />
+          <TableRow
+            songTitle='アンチジョーカー'
+            artistName='マイキP'
+            songUrl='https://www.nicovideo.jp/watch/sm37478808'
+          />
+          <TableRow
+            songTitle='potatoになっていく'
+            artistName='Neru'
+            songUrl='https://www.nicovideo.jp/watch/sm36620941'
+          />
        </table>
     </div>
   );
--- src/TableRow.js
+++ src/TableRow.js
@@ -1,6 +1,6 @@
-export default function TableRow(songTitle, artistName, songUrl) {
+export default function TableRow({songTitle, artistName, songUrl}) {
   return (
-    <tr>
+    <tr style={{ height: '36px', textAlign: 'center', paddingBottom: '16px', border: 'solid 1px #333'}}>
       <td>{songTitle}</td>
       <td>{artistName}</td>
       <td><a href={songUrl}>{songUrl}</a></td>
